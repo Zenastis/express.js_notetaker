9346989fc85275d3fca6df2d59ae3d3a9589de28
samuel mcswain <samuelmcswain@hotmail.com>
2021-04-18 16:31:48 -0400

    store.js finished

--- db/store.js
+++ db/store.js
@@ -18,7 +18,34 @@ class store {
         return this.read().then((notes) => {
             let parsedNotes;
 
-            try 
-        })
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error("note 'title' and 'text' can't be blank");
+        }
+
+        const newNote = { title, text, id };
+
+        return this.getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote);
+    }
+
+    removeNote(id) {
+        return this.getNotes()
+        .then((notes) => notes.filter((notes) => note.id !== id))
+        .then((filteredNotes) => this.write(filteredNotes));
     }
 }
+
+module.exports = new store();
